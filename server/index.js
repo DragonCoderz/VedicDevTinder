8dc9dde135cefc3994dfb9a4c3f55b4537822c43
deveshkakkar <devesh.kakkar@gatech.edu>
2022-09-12 03:19:23 -0400

    migrate backend and front end together

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.post('/signup', async (req, res) => {
         const database = client.db('app-data')
         const users = await database.collection('users')
 
-        const exisitingUser = await users.findOne({ email})
+        const exisitingUser = await users.findOne({ email })
 
         if (exisitingUser) {
             return res.status(409).send('User already exists! Please login instead')
@@ -50,6 +50,7 @@ app.post('/signup', async (req, res) => {
 
     } catch (err) {
         console.log(err)
+        console.log('Error connecting to database')
     } finally {
         await client.close()
     }
@@ -154,6 +155,46 @@ app.get('/gendered-users', async (req, res) => {
         const database = client.db('app-data')
         const users = await database.collection('users')
         const query = {gender_identity: {$eq: gender}}
+
+        const foundUsers = await users.find(query).toArray()
+        res.json(foundUsers)
+    } finally {
+        await client.close()
+    }
+})
+
+app.put('/user', async (req, res) => {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    const formData = req.body.formData
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const query = {user_id: formData.user_id}
+
+        const updateDocument = {
+            $set: {
+                first_name: formData.first_name,
+                dob_day: formData.dob_day,
+                dob_month: formData.dob_month,
+                dob_year: formData.dob_year,
+                show_gender: formData.show_gender,
+                gender_identity: formData.gender_identity,
+                gender_interest: formData.gender_interest,
+                url: formData.url,
+                about: formData.about,
+                matches: formData.matches
+            },
+        }
+
+        const insertedUser = await users.updateOne(query, updateDocument)
+
+        res.json(insertedUser)
+
+    } finally {
+        await client.close()
     }
 })
 
