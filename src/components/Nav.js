14913dfafb51335c0a7f8657546c0801c34784d2
VedicPanda <vedic.panda@gmail.com>
2022-09-11 23:24:19 -0400

    Created a dashboard UI and also fixed the UI in the home page that deleted the log in button.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,7 @@
 import logo from "../images/tinder_logo_white.png"
 import colorLogo from "../images/color-logo-tinder.png"
 
-const Nav = ({minimal, setShowModal, showModal, setIsSignUp}) => {
-
-    const authToken = true;
+const Nav = ({minimal, authToken, setShowModal, showModal, setIsSignUp}) => {
 
     const handleClick = () => {
         setShowModal(true)
